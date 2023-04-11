4a6ecb5459ee86b35714e43535215be74376364a
xiao <42403720+Jarvis-depot@users.noreply.github.com>
2023-04-11 11:46:15 +0800

    Update parse_pdf_new.py

--- parse_pdf_new.py
+++ parse_pdf_new.py
@@ -1,7 +1,7 @@
 import pdfplumber
 
 # Open the PDF file
-with pdfplumber.open("../pdf/Audio.pdf") as pdf:
+with pdfplumber.open("../pdf/<name>.pdf") as pdf:
     # Create an empty string to store the text
     text = ""
     # Loop through each page of the PDF
@@ -10,7 +10,7 @@ with pdfplumber.open("../pdf/Audio.pdf") as pdf:
         text += page.extract_text()
 
 # Save the text to a file
-with open("../pdf/Audio_new.txt", "w") as file:
+with open("../pdf/<name>.txt", "w") as file:
     file.write(text)
 
 # The above code uses pdfplumber to open the PDF file located at "../pdf/Audio.pdf", extracts the text from each page of the PDF, and saves the text to a file located at "../pdf/Audio_new.txt".
